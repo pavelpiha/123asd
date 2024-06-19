93e1ebfbd7c606643b9829e964f716dea8a7454d
Pavel-Piha <ppiha@infoblox.com>
2024-06-19 19:47:50 +0200

    feat: add new component

--- 1.ts
+++ 1.ts
@@ -1,12 +1,12 @@
 @Component({
-  imports [MatButtonModule, MatIconModule, MatTreeModule],
-  selector app-tree-view,
-  templateUrl tree-view.component.html,
-  styleUrls [tree-view.component.scss],
-  standalone true,
+  imports: [MatButtonModule, MatIconModule, MatTreeModule],
+  selector: 'app-tree-view',
+  templateUrl: 'tree-view.component.html',
+  styleUrls: ['tree-view.component.scss'],
+  standalone: true,
 })
 export class TreeViewComponent {
-  rawData string[] = [];
-   private _transformer = (node string) {}
-  dataSource string;
+  raw_data string: string[] = [];
+  // private _transformer = (node string) = {}
+  dataSource: string;
 }
