09c353f169572d0e609e63ae0082fee251db76f4
Pavel-Piha <ppiha@infoblox.com>
2024-06-20 08:46:52 +0200

    chore: refactor

--- .github/scripts/analyze-pull-request.ts
+++ .github/scripts/analyze-pull-request.ts
@@ -17,10 +17,10 @@ function createBasePullRequestPrompt(
   pullRequestDetails: PullRequestDetails
 ): string {
   return `Your task is to check pull request follows style-guide. Instructions:
-  - Provide the response in following JSON format: "Style guide: <new line> <violated_rule> <status> new line for each rule."
-  - Example: "Style guide: <new line> <rule_name> <status> <new line> <rule_name> <status>"
+  - Provide the response in following JSON format: "<Style guide Title> \n <style_guide_section>\n  <violated_rule> <status> \n"
+  - Example: "Checker Style guide: \n Section 1. \n Use camel case :x:\n"
   - Go through each rule strictly and carefully.
-  - Provide a list of violated rules as a bullet point Exactly as it appears in the Style Guide (not only the number or rule but the name as well) followed by the <status>. ':x:'- if it is violated; ':white_check_mark:' - if it is not violated; ':confused:' - if you are unsure
+  - Provide a list of violated rules exactly as it appears in the Style Guide followed by the <status>. ':x:'- if rule is violated; ':confused:' - if you are unsure
   - Be especially careful when checking the branch and commit rules, as you have made mistakes in this area before.
   - IMPORTANT: NEVER provide any explanations or code in your response.
 
