2817b4d80b7ad7fb308adaaeebd483fe12240075
Pavel-Piha <ppiha@infoblox.com>
2024-06-20 09:14:04 +0200

    another comment

---
+++ 1.ts
@@ -0,0 +1,12 @@
+@Component({
+  imports: [MatButtonModule],
+  selector: 'app-tree-view',
+  templateUrl: 'tree-view.component.html',
+  styleUrls: ['tree-view.component.scss'],
+  standalone: true,
+})
+export class TreeViewComponent {
+  raw_data: string[] = [];
+  // private _transformer = (node string) = {}
+  dataSource: string;
+}
