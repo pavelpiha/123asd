4172c404164c904cb476b2899ee9ba433a12d0bb
Pavel-Piha <ppiha@infoblox.com>
2024-06-21 13:14:03 +0200

    asdfadf

---
+++ plane-view.component.ts
@@ -0,0 +1,9 @@
+@Component({
+  selector: 'app-plane-view',
+  templateUrl: 'tree-plane.component.html',
+  styleUrls: ['tree-plane.component.scss'],
+})
+export class PlaneViewComponent {
+  rawData: string[] = [];
+  dataSource: string;
+}
